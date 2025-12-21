4026255cf7dd5ecf8b9bd2ba581ee59783e3cd1c
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-21 15:07:00 +0300

    Add FileWriter and Formatter logic for output

--- lab9Assignment1.java
+++ lab9Assignment1.java
@@ -1,6 +1,9 @@
-import java.io.IOException;
-import java.nio.file.Paths;
+import java.io.IOException;//To write IOException I suppose?
+import java.nio.file.Paths;//To use Paths.get
 import java.util.Scanner;
+import java.util.Formatter;//To write a file -- Formatter f = null;  f = new Formatter ("people.txt");
+import java.io.FileWriter;//Hm I think formatter makes a txt file and fileWriter writes in that txt file -
+                          //fw=new FileWriter("word_stats.txt", true);   FileWriter fw = null;
 
 public class lab9Assignment1 {
     public static void main(String[] args) {
@@ -9,42 +12,46 @@ public class lab9Assignment1 {
         System.out.println("Enter the file path: (example: q1.txt)");
         String filePath = sc.nextLine();
 
-        int maxWords = 10000;
-
-        int[] ints=new int[maxWords];
-
         int totalWords=0;
+        String[] uniqueWords = new String[1000];
         int totalUniqueWords=0;
 
         switch (filePath){
-            case "q1.txt": {
+            case "q1.txt","q1": {
                 Scanner reader = null;
+                Formatter f = null; //java.util.Formatter
+                FileWriter fw = null; //java.io.FileWriter
                 try {
                     reader = new Scanner(Paths.get("q1.txt"));
                     while (reader.hasNextLine()) {
-                        String[] words = reader.nextLine().split(" ");
-
+                        String line = reader.nextLine();
+                        if (line.trim().isEmpty()) continue;
+                        String[] words = reader.nextLine().split(" ");//Briefly when reader sees a "space" it takes the part before to the array
                         for (int i = 0; i < words.length; i++) {
                             words[i] = words[i].replace(",", "");
                             words[i] = words[i].replace(".", "");
                             words[i] = words[i].toLowerCase();
-                            System.out.println(words[i]);
+                            System.out.print(words[i]+" ");
                             totalWords++;
-                        }
-                        System.out.println("--------------");
-                    }
-                    System.out.println(totalWords);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (reader != null) {
-                        reader.close();
+                        }
                     }
+                    fw = new FileWriter("word_stats.txt", true);
+                    f = new Formatter(fw); //For preventing collision
+                    f.format("Total words: "+totalWords);
+                    fw.append("\nTesting for appending.");
+                } catch (IOException e ) {
+                    e.printStackTrace();
+                    System.out.println("Something Went Wrong...");
+                } finally {
+                    if (reader != null)reader.close();
+                    if (f!= null) f.close();
                 }
+                break;
             }
             default: {
-                System.out.println("Please enter a valin input (q1.txt - q2.txt - q3.txt");
+                System.out.println("Please enter a valid input (q1.txt - q2.txt - q3.txt)");
+                break;
             }
         }
     }
