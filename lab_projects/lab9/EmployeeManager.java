69f000f5f66e922fbb878ab7c5c110f9d0cbf863
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-21 21:34:06 +0300

    Add logic to parse employees from text file into array

---
+++ lab_projects/lab9/EmployeeManager.java
@@ -0,0 +1,40 @@
+package lab_projects.lab9;
+import java.io.File;
+import java.io.IOException;//"src/lab_projects/lab9/q3.txt")
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.Formatter;
+import java.io.FileWriter;
+
+public class EmployeeManager {
+        Employee[] employees = new Employee[2];;
+        Scanner reader = null;
+        Formatter f = null;
+        FileWriter fw = null;
+
+        public void loadEmployeesFromFile(String filePath){
+            int index=0;
+            try{
+                reader = new Scanner(Paths.get(filePath));
+                while(reader.hasNextLine()&&index<employees.length){
+                    String line = reader.nextLine();
+                    if(line.trim().isEmpty()) continue;
+                    String[] words = line.split(" ");
+                    if(words.length>=3){
+                        String name = words[0];
+                        String department = words[1];
+                        String email = words[2];
+
+                        Employee newEmployee = new Employee(name, department, email);
+                        employees[index] =  newEmployee;
+                        index++;
+                    }
+                }
+            }catch (IOException e){e.printStackTrace();}
+            finally {
+                if(reader != null){reader.close();}
+                if(f != null){f.close();}
+            }
+        }
+    }
+
