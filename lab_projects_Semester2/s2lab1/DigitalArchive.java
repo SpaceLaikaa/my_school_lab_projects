fe412a0fdf5f9da2de1969c9fa2ffe263038af18
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-02-21 14:55:35 +0300

    Add Librarian class to add and remove books from a book Array.

--- lab_projects_Semester2/s2lab1/DigitalArchive.java
+++ lab_projects_Semester2/s2lab1/DigitalArchive.java
@@ -4,7 +4,10 @@ package lab_projects_Semester2.s2lab1;
 //MAIN CLASS
 public class DigitalArchive {
     public static void main (String[] args){
-        Book book = new Book(978935300,"Animal Farm",280,10);
-        book.displayBooks();
+        Book[] books = new Book[5];
+        Book book1 = new Book(978935300,"Animal Farm",280,10);
+        Book book2 = new Book(95794504,"Dorian Gray's Portrait",350,22);
+        book1.displayBooks();
+
     }
 }
--- lab_projects_Semester2/s2lab1/Librarian.java
+++ lab_projects_Semester2/s2lab1/Librarian.java
@@ -1,5 +1,30 @@
 package lab_projects_Semester2.s2lab1;
 
 public class Librarian {
+    private int employeeID;
+    private String name;
+    private int age;
+    private Book[] books;
+
+    public Librarian(int employeeID, String name, int age,Book[] comingBooks){
+        this.employeeID = employeeID;
+        this.name=name;
+        this.age = age;
+        this.books = new Book[5];
+
+        int boundary=comingBooks.length;
+        if(boundary>5){
+            boundary=5;
+        }
+        for(int i =0;i< boundary;i++){
+            this.books[i] = comingBooks[i];
+        }
+    }
+    public void addBook(Book[] addBook){
+
+    }
+    public void removeBook(String isbn){
+
+    }
 
 }
