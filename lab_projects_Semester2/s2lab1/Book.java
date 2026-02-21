533c27fafd4a6e11b4b73c1ae12a65813b5fef6f
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-02-21 12:54:28 +0300

    Implemented setters and getters for Book.java variables

--- lab_projects_Semester2/s2lab1/Book.java
+++ lab_projects_Semester2/s2lab1/Book.java
@@ -16,4 +16,15 @@ public class Book {
     public void displayBooks(){
         System.out.printf("ISBN: %d | Title: %s | Page Count: %d | Price: $%d",isbn,title,pageCount,price);
     }
+
+    //IDK if We are going to use setter and getter methods but lab Wants it
+    public long getISBN(){return isbn;}
+    public String getTitle(){return title;}
+    public int getPageCount(){return pageCount;}
+    public int getPrice(){return price;}
+
+    public void setISBN(long isbn){this.isbn = isbn;}
+    public void setTitle(String title){this.title = title;}
+    public void setPageCount(int pageCount){this.pageCount = pageCount;}
+    public void setPrice(int price){this.price = price;}
 }
