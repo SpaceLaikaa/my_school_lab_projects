4718c12d3a581ae72f86464d32c666a088ddd9fb
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-02-27 13:00:21 +0300

    Add simple deposit and withdraw methods.

--- lab_projects_Semester2/s2lab2/Bank.java
+++ lab_projects_Semester2/s2lab2/Bank.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        BankAccount bankAccount1 = new BankAccount("Arda", 12345678912);
+        BankAccount bankAccount1 = new BankAccount("Arda", 12345678912L);
         System.out.println("-----------------");
         System.out.println("1. Deposit"); System.out.println("2. Withdraw");
         System.out.println("3. Check Balance"); System.out.println("4. Exit");
@@ -19,6 +19,12 @@ public class Bank {
             System.out.println("How much money do you want to deposit ?");
             int userDepositChoice = sc.nextInt();
             bankAccount1.deposit(userDepositChoice);
+        }
+        else if(userChoice.equalsIgnoreCase("2")||userChoice.equalsIgnoreCase("withdraw")){
+            System.out.println("Balance: "+bankAccount1.getBalance());
+            System.out.println("How much money do you want to withdraw?");
+            double userWithdrawChoice = sc.nextInt();
+            bankAccount1.withdraw(userWithdrawChoice);
         }
 
     }
@@ -32,15 +38,14 @@ public class Bank {
 
         public BankAccount(String ownerName, long iban) {
             String stringIban = iban+"";
-            if( stringIban.length() == 11){
+            if(stringIban.length() == 11){
                 this.balance = 0;
                 this.iban = iban;
                 this.ownerName = ownerName;
             }
             else{
-                System.out.println("Please enter a valid IBAN");
+                System.out.println("Please enter a valid IBAN (11 decimal)");
             }
-
         }
 
         public void setBalance(double balance){this.balance = balance;}
@@ -51,8 +56,13 @@ public class Bank {
         public double getBalance(){return balance;}
         public String getOwnerName(){return ownerName;}
 
-        public void deposit(long amount){
-
+        public void deposit(double depositAmount){
+            double newBalanceDeposit = getBalance()+depositAmount;
+            System.out.println(depositAmount+"$ added to your account.\nNew Balance: "+newBalanceDeposit);
+        }
+        public void withdraw(double withdrawAmount){
+            double newBalanceWithdraw = getBalance()-withdrawAmount;
+            System.out.println(withdrawAmount+"$ withdrawed from your account. \nNew Balance: "+newBalanceWithdraw);
         }
     }
 
